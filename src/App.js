05daf11138a8026a6a48c1b46ff0ff58326ecf0d
Juan Rene Soler Siri <juanr.soler_93@hotmail.es>
2021-04-05 23:17:55 -0400

    add list exercise

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
-import ToggleClock from "./components/ToggleClock";
+//import ToggleClock from "./components/ToggleClock";
+import OneToFiveList from "./components/OneToFiveList";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <label><ToggleClock /></label>
+        <label><OneToFiveList /></label>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
